ad5dc0646c5cc38fd16654be1d350ef51bc2b1fb
claudiobailon <c.bailonschubert@gmail.com>
2020-09-14 17:53:07 -0700

    Cleaned up code

--- basics/Main.java
+++ basics/Main.java
@@ -3,8 +3,7 @@ import java.time.format.DateTimeFormatter;
 // import java.time.*;
 
 public class Main{
-  public static void main(String[] args)
-  {
+  public static void main(String[] args){
     ////////Plurzlize function calls//////
     int dogCount =1;
     String dog = pluralize("dog", dogCount);
@@ -22,32 +21,27 @@ public class Main{
     clock();
   }
 //////////// Pluralize Function///////////////////
-  public static String pluralize(String word, int num)
-  {
-    if(num > 1 || num == 0)
-    {
+  public static String pluralize(String word, int num){
+    if(num > 1 || num == 0){
       return word + 's';
-    }else {
+    }else{
       return word;
     }
 
   }
 /////////////// FlipNHeads Function////////
-  public static int flipNHeads(int n)
-  {
+  public static int flipNHeads(int n){
     int attempts = 0;
     int inARow = 0;
 
     while (inARow < n){
       
       int num = (Math.random() <= 0.5)? 0: 1; //David Dicken help figured this out
-      if (num == 0)
-      {
+      if (num == 0){
         System.out.println("Tails");
         attempts ++;
         inARow = 0;
-      }else
-      {
+      }else{
         System.out.println("Heads");
         attempts ++;
         inARow ++;
@@ -55,27 +49,23 @@ public class Main{
       }
       
     }
-    if (inARow == n)
-    {
+    if (inARow == n){
       System.out.println("It took " + attempts + " flips to flip " + n + " heads in a row.");
     }
     return attempts;
   }
 ////////////Command Line Clock//////////
-  public static void clock()
-  {
+  public static void clock(){
     int num = 0;
     LocalDateTime now = LocalDateTime.now();
     String time = now.format(DateTimeFormatter.ofPattern("hh:mm:ss"));
     String newTime = now.format(DateTimeFormatter.ofPattern("hh:mm:ss"));
 
-    while(num == 0)
-    {
+    while(num == 0){
       now = LocalDateTime.now();
       newTime = now.format(DateTimeFormatter.ofPattern("hh:mm:ss"));
 
-      if(!time.equals(newTime))
-      {
+      if(!time.equals(newTime)){
         time = now.format(DateTimeFormatter.ofPattern("hh:mm:ss"));
         System.out.println(time);
       }
