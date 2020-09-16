ea23ec497b03480b421fbdb5128f2c38e7dc1540
claudiobailon <c.bailonschubert@gmail.com>
2020-09-15 20:44:15 -0700

    Tests pass

--- basiclibrary/src/main/java/basiclibrary/Roll.java
+++ basiclibrary/src/main/java/basiclibrary/Roll.java
@@ -1,7 +1,15 @@
 package basiclibrary;
 
+import java.util.Arrays;
+
 public class Roll {
-    public int[] roll(int i) {
-        return new int[0];
+    public int[] roll(int rollNum) {
+        int[] diceRolls = new int[rollNum];
+        for ( int i = 0; i < rollNum; i++){
+            int diceSide = (int) (Math.random() * (7 - 1) )+ 1;//https://stackoverflow.com/questions/5271598/java-generate-random-number-between-two-given-values
+            diceRolls[i] = diceSide;
+        }
+        System.out.println(Arrays.toString(diceRolls));
+        return diceRolls;
     }
 }
--- basiclibrary/src/test/java/basiclibrary/RollTest.java
+++ basiclibrary/src/test/java/basiclibrary/RollTest.java
@@ -2,31 +2,36 @@ package basiclibrary;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class RollTest {
         @Test public void testRollLength() {
             Roll roll = new Roll();
-            int[] rolls = roll.roll(5);
-            int[] rolls2 = roll.roll(8);
+            int rollNum1 = 5;
+            int rollNum2 = 8;
+            int[] rolls = roll.roll(rollNum1);
+            int[] rolls2 = roll.roll(rollNum2);
             int arrLen = rolls.length;
             int arrLen2 = rolls2.length;
 
-            assertEquals(arrLen, 5);
-            assertEquals(arrLen2, 8);
+            assertEquals(arrLen, rollNum1);
+            assertEquals(arrLen2, rollNum2);
         }
         @Test public void testRollContent(){
             Roll roll = new Roll();
             int[] rolls = roll.roll(25);
-
             boolean oneThroughSix = false;
+
             for(int i = 0; i < rolls.length; i++){
-                if(rolls[i] == 1 || rolls[i] == 2 || rolls[i] == 3 || rolls[i] == 4 || rolls[i] == 5 || rolls[i] == 6){
+                if(rolls[i] > 0 && rolls[i] < 7){
                     oneThroughSix = true;
+
+                }else{
+                    oneThroughSix = false;
+                    break;
                 }
             }
-            assertTrue("All numbers were between 1 and 6",oneThroughSix);
+            assertTrue("All numbers were between 1 and 6", oneThroughSix);
         }
 
 }
