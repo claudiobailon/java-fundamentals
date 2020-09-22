659d08f0cc49682aeed634186afcddb93710b5a8
claudiobailon <c.bailonschubert@gmail.com>
2020-09-21 23:09:49 -0700

    Review class made and tests pass

--- inheritance/src/main/java/inheritance/Review.java
+++ inheritance/src/main/java/inheritance/Review.java
@@ -1,5 +1,17 @@
 package inheritance;
 
 public class Review {
+    String body;
+    String author;
+    int stars;
 
+    public Review(String body, String author, int stars){
+        this. body = body;
+        this.author = author;
+        this.stars = stars;
+    }
+    public String toString(){
+        String output = String.format("\nBody: %s\n By: %s\n Stars: %d\n", body, author, stars);
+        return output;
+    }
 }
--- inheritance/src/test/java/inheritance/ReviewTest.java
+++ inheritance/src/test/java/inheritance/ReviewTest.java
@@ -1,4 +1,24 @@
 package inheritance;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class ReviewTest {
+    @Test
+    public void testSomeLibraryMethod() {
+        assertTrue(true);
+    }
+    @Test public void testRevConstructor(){
+        Review newReview = new Review("Pancakes are great, but avoid the BLT", "Mary N",4);
+        assertEquals("Pancakes are great, but avoid the BLT", newReview.body);
+        assertEquals("Mary N", newReview.author);
+        assertEquals(4, newReview.stars);
+    }
+    @Test public void testRevString(){
+        Review newReview = new Review("Pancakes are great, but avoid the BLT", "Mary N",4);
+        assertEquals("\nBody: Pancakes are great, but avoid the BLT\n By: Mary N\n Stars: 4\n", newReview.toString());
+        System.out.println(newReview.toString());
+
+    }
 }
